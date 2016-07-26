ce20a130bd10d8b4c8dd596e33b9aefe3cbcf328
xiamwei <xiamwei@hotmail.com>
2016-07-26 09:43:23 +0800

    Initial Commit

---
+++ yingke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yingke
+//
+//  Created by Sino on 16/7/26.
+//  Copyright © 2016年 夏明伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yingke/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yingke
+//
+//  Created by Sino on 16/7/26.
+//  Copyright © 2016年 夏明伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
